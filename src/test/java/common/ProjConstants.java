743259691612ff711ab0e9c18da546b84959ec5d
alistairzhu <alistairzhu@gmail.com>
2018-10-07 11:48:09 -0400

    first commit a

---
+++ src/test/java/common/ProjConstants.java
@@ -0,0 +1,6 @@
+package common;
+
+public class ProjConstants {
+    public static final String BLOCKEXPLORER_URL ="https://blockexplorer.com/api/";
+    public static final String GDAX_URL = "https://api.gdax.com/";
+}
