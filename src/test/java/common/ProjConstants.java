d0b23f4c24370454b77c319f0630d70e4c718d7c
alistairzhu <alistairzhu@gmail.com>
2018-10-07 11:54:36 -0400

    Revert "first commit a"
    This reverts commit 743259691612ff711ab0e9c18da546b84959ec5d.

--- src/test/java/common/ProjConstants.java
+++
@@ -1,6 +0,0 @@
-package common;
-
-public class ProjConstants {
-    public static final String BLOCKEXPLORER_URL ="https://blockexplorer.com/api/";
-    public static final String GDAX_URL = "https://api.gdax.com/";
-}
